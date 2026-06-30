476dc3e975540a3e72b1aadc418d7fe84e29a751
Mahi Thakur <100193737+themahipalt@users.noreply.github.com>
2026-06-30 20:20:13 +0530

    Time: 21 ms (34.48%) | Memory: 43.2 MB (84.48%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cs
@@ -0,0 +1,28 @@
+public class Solution
+{
+    public int NumberOfSubstrings(string s)
+    {
+        int left = 0;
+        int right = 0;
+        int count = 0;
+
+        int[] freq = new int[3];
+
+        while (right < s.Length)
+        {
+            freq[s[right] - 'a']++;
+
+            while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0)
+            {
+                count += s.Length - right;
+
+                freq[s[left] - 'a']--;
+                left++;
+            }
+
+            right++;
+        }
+
+        return count;
+    }
+}
